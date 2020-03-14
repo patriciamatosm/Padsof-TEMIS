9ca701385c7634f8ceae4273920482aba3ef705d
patriciamatosm <patriciamatosm@172.17.69.250>
2020-03-14 13:20:06 +0100

    Temis y usuario: empezando tests

---
+++ test/TemisTest.java
@@ -0,0 +1,33 @@
+
+import org.junit.Test;
+
+
+public class TemisTest {
+    @Test
+    public void escribirFichero() {
+    }
+
+    @org.junit.Test
+    public void leerFichero() {
+    }
+
+    @org.junit.Test
+    public void registrarse() {
+    }
+
+    @org.junit.Test
+    public void iniciaSesion() {
+    }
+
+    @org.junit.Test
+    public void cierraSesion() {
+    }
+
+    @org.junit.Test
+    public void anadirProyecto() {
+    }
+
+    @org.junit.Test
+    public void anadirColectivo() {
+    }
+}
