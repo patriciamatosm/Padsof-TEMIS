7cffbb198f4cc423983d1571390f79e248b84059
dprieto24 <daniel.prietof@estudiante.uam.es>
2020-05-04 21:00:37 +0200

    comentarios de nuevo en temis

--- src/mvc/model/Temis.java
+++ src/mvc/model/Temis.java
@@ -440,6 +440,13 @@ public class Temis {
         this.colectivos.put(c.getNombre(), c);
     }
 
+    /**
+     * Funcion que crea un colectivo y comprueba que no existe uno con el mismo nombre
+     * @param nombre del colectivo
+     * @param desc del colectivo
+     * @return nuevo colectivo
+     * @throws Exception si ya existe uno con el mismo nombre o el crear el colectivo manda una excepcion
+     */
     public Colectivo crearCol(String nombre, String desc) throws Exception {
         for(Colectivo c : this.colectivos.values()) {
             if(c.getNombre().equals(nombre)) {
@@ -455,7 +462,18 @@ public class Temis {
         return c;
     }
 
+    /**
+     * Funcion que crea un subcolectivo del colectivo que se esta representando
+     * @param descripcion del nuevo colectivo
+     * @param nombre del nuevo colectivo
+     * @throws Exception si ya existe un colectivo con ese nombre o el constructor de colectivo manda una
+     */
     public void crearSubColectivo(String descripcion, String nombre) throws Exception {
+        for(Colectivo c : this.colectivos.values()) {
+            if(c.getNombre().equals(nombre)) {
+                throw new Exception("Ya existe un colectivo con ese nombre");
+            }
+        }
         for(Colectivo c : this.colectivos.values()) {
             if(c.getNombre().equals(colRepresentado.getNombre())) {
                 c.crearSubcolectivo(nombre, descripcion);
@@ -463,6 +481,10 @@ public class Temis {
         }
     }
 
+    /**
+     * Funcion que une al usuario conectado al colectivo c
+     * @param c colectivo al que se quiere unir
+     */
     public void unirse(Colectivo c) {
         for(Colectivo col : this.colectivos.values()) {
             if(col.getNombre().equals(c.getNombre())) {
@@ -475,6 +497,10 @@ public class Temis {
         }
     }
 
+    /**
+     * Funcion que hace que el usuario conectado deje de seguir al colectivo
+     * @param c colectivo que abandonar
+     */
     public void abandonar(Colectivo c) {
         for(Colectivo col : this.colectivos.values()) {
             if(col.getNombre().equals(c.getNombre())) {
